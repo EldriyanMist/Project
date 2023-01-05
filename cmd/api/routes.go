072addbe283a6961dd0e243d4a4710f61438b99b
Damir <kenzhe.damir17@gmail.com>
2023-01-05 13:14:28 +0600

    778

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,8 +12,8 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	// Require a PATCH request, rather than PUT.
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
-	// Add the route for the DELETE /v1/movies/:id endpoint.
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	return router
 }
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -60,25 +60,19 @@ RETURNING id, created_at, version`
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
-	// The PostgreSQL bigserial type that we're using for the movie ID starts
-	// auto-incrementing at 1 by default, so we know that no movies will have ID values
-	// less than that. To avoid making an unnecessary database call, we take a shortcut
-	// and return an ErrRecordNotFound error straight away.
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	// Define the SQL query for retrieving the movie data.
+	// Update the query to return pg_sleep(10) as the first value.
 	query := `
-SELECT id, created_at, title, year, runtime, genres, version
+SELECT pg_sleep(10), id, created_at, title, year, runtime, genres, version
 FROM movies
 WHERE id = $1`
-	// Declare a Movie struct to hold the data returned by the query.
 	var movie Movie
-	// Execute the query using the QueryRow() method, passing in the provided id value
-	// as a placeholder parameter, and scan the response data into the fields of the
-	// Movie struct. Importantly, notice that we need to convert the scan target for the
-	// genres column using the pq.Array() adapter function again.
+	// Importantly, update the Scan() parameters so that the pg_sleep(10) return value
+	// is scanned into a []byte slice.
 	err := m.DB.QueryRow(query, id).Scan(
+		&[]byte{}, // Add this line.
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -87,9 +81,6 @@ WHERE id = $1`
 		pq.Array(&movie.Genres),
 		&movie.Version,
 	)
-	// Handle any errors. If there was no matching movie found, Scan() will return
-	// a sql.ErrNoRows error. We check for this and return our custom ErrRecordNotFound
-	// error instead.
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -98,29 +89,37 @@ WHERE id = $1`
 			return nil, err
 		}
 	}
-	// Otherwise, return a pointer to the Movie struct.
 	return &movie, nil
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	// Declare the SQL query for updating the record and returning the new version
-	// number.
+	// Add the 'AND version = $6' clause to the SQL query.
 	query := `
 UPDATE movies
 SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-WHERE id = $5
+WHERE id = $5 AND version = $6
 RETURNING version`
-	// Create an args slice containing the values for the placeholder parameters.
 	args := []any{
 		movie.Title,
 		movie.Year,
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version, // Add the expected movie version.
 	}
-	// Use the QueryRow() method to execute the query, passing in the args slice as a
-	// variadic parameter and scanning the new version value into the movie struct.
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	// Execute the SQL query. If no matching row could be found, we know the movie
+	// version has changed (or the record has been deleted) and we return our custom
+	// ErrEditConflict error.
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
